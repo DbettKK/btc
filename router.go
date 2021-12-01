4399b48f9dfb217bc32abddbb37dbcf0577d0829
ubuntu <ubuntu@localhost.localdomain>
2021-12-01 15:34:29 +0800

    modify

--- router.go
+++ router.go
@@ -18,7 +18,7 @@ func cors() gin.HandlerFunc {
 		method := c.Request.Method               //请求方法
 		origin := c.Request.Header.Get("Origin") //请求头部
 		var headerKeys []string                  // 声明请求头keys
-		for k, _ := range c.Request.Header {
+		for k := range c.Request.Header {
 			headerKeys = append(headerKeys, k)
 		}
 		headerStr := strings.Join(headerKeys, ", ")
